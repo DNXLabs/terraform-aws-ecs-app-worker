da733e0e6425cbd2d7ac59e4feac405c609fea15
José Roberto <31750084+jrpradojr@users.noreply.github.com>
2021-05-27 22:19:55 -0300

    Added the possibility of putting the placement_constraints and ordered_placement_strategy (#4)
    * Added to the possibility of putting the placement_constraints and placement_constraints
    * Changed variable placement_constraints to list(object)
    * Changed variable ordered_placement_strategy to list(object)

--- ecs-service.tf
+++ ecs-service.tf
@@ -8,6 +8,21 @@ resource "aws_ecs_service" "default" {
   deployment_maximum_percent         = 100
   deployment_minimum_healthy_percent = 0
 
+  dynamic "placement_constraints" {
+    for_each = var.placement_constraints
+    content {
+      expression = lookup(placement_constraints.value, "expression", null)
+      type       = placement_constraints.value.type
+    }
+  }
+
+  dynamic "ordered_placement_strategy" {
+    for_each = var.ordered_placement_strategy
+    content {
+      field = lookup(ordered_placement_strategy.value, "field", null)
+      type  = ordered_placement_strategy.value.type
+    }
+  }
   lifecycle {
     ignore_changes = [task_definition]
   }
